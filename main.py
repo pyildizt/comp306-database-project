4411989cf5d74db37855280b5f89df282c76aab2
beyzaturkmen21 <bturkmen21@ku.edu.tr>
2024-01-22 20:56:14 +0300

    c

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import pandas as pd
 db_connection = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="123678zulal", 
+  passwd="4567-bbbcomp", 
   auth_plugin='mysql_native_password'
 )
 db_cursor = db_connection.cursor(buffered=True)
