06076cb26258f71d7710044fd3d5370a1fb0a931
Alper Şahin <alpersahin21@ku.edu.tr>
2024-01-22 19:23:21 +0300

    minor fix

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import pandas as pd
 db_connection = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="z1x?1zKucs",
+  passwd="mysql201468",
   auth_plugin='mysql_native_password'
 )
 db_cursor = db_connection.cursor(buffered=True)
